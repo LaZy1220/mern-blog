9f8fa231fe8adad3fa6ab0d621539f378362b443
LaZy1220 <4elove4iska@gmail.com>
2023-02-21 14:19:57 +0300

    connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(8000, () => {
-  console.log("server started");
-});
+//Constants
+const PORT = process.env.PORT || 8001;
+const IP = process.env.IP;
+const DB_NAME = process.env.DB_NAME;
+
+mongoose.set("strictQuery", false);
+
+async function start() {
+  try {
+    await mongoose
+      .connect(`mongodb://${IP}/${DB_NAME}`)
+      .then(() => console.log("DB OK!!!"));
+    app.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log();
+  }
+}
+start();
